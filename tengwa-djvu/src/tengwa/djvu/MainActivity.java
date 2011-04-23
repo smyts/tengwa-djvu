59db2dc396edf4f70545e51b5c745e2232459d6a
Veronika Levkevich <veronika.levkevich@gmail.com>
2011-04-23 18:42:54 +0300

    Initial commit for TengwaDjvu

---
+++ tengwa-djvu/src/tengwa/djvu/MainActivity.java
@@ -0,0 +1,14 @@
+package tengwa.djvu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
