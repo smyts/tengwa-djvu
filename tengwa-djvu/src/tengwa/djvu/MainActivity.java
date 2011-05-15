59a5bbee5298d2e2e5f2c7182cbea28b323cc0ea
Veronika Levkevich <veronika.levkevich@gmail.com>
2011-05-15 11:50:15 +0300

    MainActivity appearence is predefined. No icons for navigation buttons, no icons for menu items, textview stub.

--- tengwa-djvu/src/tengwa/djvu/MainActivity.java
+++ tengwa-djvu/src/tengwa/djvu/MainActivity.java
@@ -2,13 +2,44 @@ package tengwa.djvu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setTitle(R.string.app_name);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+        case R.id.menu_item_about:
+            break;
+        case R.id.menu_item_open:
+            break;
+        case R.id.menu_item_goto:
+            break;
+        case R.id.menu_item_settings:
+            break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){ }
+
+    @Override
+    protected void onPause() {}
+
+    @Override
+    protected void onResume() {}
 }
