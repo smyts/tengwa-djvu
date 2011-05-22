e6bc126d4b4aac48b1c1539282628645134ba3d1
Sergei Myts <sergei.myts@gmail.com>
2011-05-22 23:57:54 +0300

    DjVulibre compilation process reworked.
    * Added partial integration with lib's default configure script,
      configure-android script was created.
    * Also, some directories necessary for the configure to work were
      added to the project.
    * TODO, ANDROID-INSTALL files with instructions and notes were added.
    * jni directory structure was modified
    * "djvulibre" library successfully compiles

--- tengwa-djvu/jni/Android.mk
+++ tengwa-djvu/jni/Android.mk
@@ -1,15 +1,1 @@
-#
-#
-# License info here
-#
-#
-#
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := djvulibre-android
-LOCAL_CFLAGS := -DANDROID_NDK
-LOCAL_SRC_FILES := $(wildcard libdjvu/*.cpp)
-
-include $(BUILD_SHARED_LIBRARY)
+include $(all-subdir-makefiles)
