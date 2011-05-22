34bb54a109ef77257af56cfc2323623cfd242326
Sergei Myts <sergei.myts@gmail.com>
2011-05-22 03:56:34 +0300

    configure-android script was added
    configure-android sets up the params for the djvulibre's ./configure file and executes it to gather settings

--- tengwa-djvu/jni/Android.mk
+++ tengwa-djvu/jni/Android.mk
@@ -10,7 +10,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := djvulibre-android
 LOCAL_CFLAGS := -DANDROID_NDK
-#LOCAL_LDLIBS := -ldjvulibre-strings
 LOCAL_SRC_FILES := $(wildcard libdjvu/*.cpp)
 
 include $(BUILD_SHARED_LIBRARY)
