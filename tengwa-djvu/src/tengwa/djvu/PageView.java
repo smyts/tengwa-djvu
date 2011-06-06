9ab2c0ce3f1383a3a69da6c0665173e3d2c4926a
Veronika Levkevich <veronika.levkevich@gmail.com>
2011-06-06 20:45:56 +0300

    Mechanism of displaying pages has been fully reworked.
    PageView doesn't create a bitmap for every change in page position
    now, it displays these changes by drawing on canvas.

--- tengwa-djvu/src/tengwa/djvu/PageView.java
+++ tengwa-djvu/src/tengwa/djvu/PageView.java
@@ -3,21 +3,21 @@ package tengwa.djvu;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 
-public class PageView extends ImageView {
-    private double mZoomFactor = 2;         /* zoom coefficient for one click on zoom button */
-    private Bitmap mOriginalPage;           /* original bitmap for current page */
-    private int mLeft;                      /* leftmost displayed pixel in original bitmap */
-    private int mTop;                       /* topmost displayed pixel in original bitmap */
+public class PageView extends View {
+    private Bitmap mPage;                   /* original bitmap for current page */
     private int mOriginalWidth;             /* width of original bitmap */
     private int mOriginalHeight;            /* height of original bitmap */
-    private int mCurrentWidth;              /* displayed width */
-    private int mCurrentHeight;             /* displayed height */
     private int mScreenWidth = 0;           /* current width of view */
     private int mScreenHeight = 0;          /* current height of view */
-    private double mCurrentZoom = 1;        /* zoom coefficient of displayed chunk of bitmap */
+
+    private double mCurrentScale = 0;       /* current scale */
+    private double mStepScale = 0.2f;       /* step scale */
+    private double mOffsetX, mOffsetY;      /* offsets for shifting page */
 
 
     public PageView(Context context) {
@@ -30,23 +30,17 @@ public class PageView extends ImageView {
     @Override
     public void onLayout(boolean changed, int left, int top, int right, int bottom){
         super.onLayout(changed, left, top, right, bottom);
-        if (changed || mScreenWidth == 0) {
-            if (mOriginalPage != null) {
-                mScreenWidth = right - left;
-                mScreenHeight = bottom - top;
-                fitPage();
-                updateImage();
-            }
-        }
+        mScreenWidth = right - left;
+        mScreenHeight = bottom - top;
     }
 
     public void loadPreferences(SharedPreferences sp){
-        double zoom = Double.parseDouble(sp.getString("zoom_factor", "2"));
-        if (zoom > 1) {
-            mZoomFactor = zoom;
+        double scale = Double.parseDouble(sp.getString("step_scale", "0.2"));
+        if (scale > 0) {
+            mStepScale = scale;
         } else {
             SharedPreferences.Editor e = sp.edit();
-            e.putString("zoom_factor", Double.toString(mZoomFactor));
+            e.putString("step_scale", Double.toString(mStepScale));
             e.commit();
         }
     }
@@ -55,148 +49,74 @@ public class PageView extends ImageView {
      * Methods for work with bitmap
      */
     public void zoomIn() {
-        if (mCurrentZoom > 1) {
-            mCurrentWidth = mOriginalWidth;
-            mCurrentHeight = mOriginalHeight;
-            mLeft = mTop = 0;
-            mCurrentZoom = 1;
-        } else {
-            int width = mCurrentWidth, height = mCurrentHeight;
-            mCurrentZoom /= mZoomFactor;
-            int xc = mLeft + width / 2, yc = mTop + height / 2;
-            width = (int) (width / mZoomFactor);
-            height = (int) (height / mZoomFactor);
-            if (width == 0 || height == 0){
-                return;
-            }
-            mLeft = xc - width / 2;
-            mTop = yc - height / 2;
-            mCurrentWidth = width;
-            mCurrentHeight = height;
-        }
-        fitPage();
-        updateImage();
+        mCurrentScale += mStepScale;
+        invalidate();
     }
 
     public void zoomOut() {
-        if (mCurrentZoom > 1) {
-            return;
+        if (mCurrentScale > mStepScale) {
+            mCurrentScale -= mStepScale;
+            invalidate();
         }
-
-        int xc = mLeft + mCurrentWidth / 2, yc = mTop + mCurrentHeight / 2;
-
-        mCurrentZoom *= mZoomFactor;
-
-        if (mCurrentZoom > 1) {
-            //zoom > 1 means showing whole page without fitting to page
-            mCurrentWidth = mOriginalWidth;
-            mCurrentHeight = mOriginalHeight;
-            mLeft = mTop = 0;
-        } else {
-            mCurrentWidth = (int) (mCurrentWidth * mZoomFactor);
-            if (mCurrentWidth > mOriginalWidth) {
-                mCurrentWidth = mOriginalWidth;
-            }
-            mCurrentHeight = (int) (mCurrentHeight * mZoomFactor);
-            if (mCurrentHeight > mOriginalHeight) {
-                mCurrentHeight = mOriginalHeight;
-            }
-
-            if (xc - mCurrentWidth / 2 < 0) {
-                mLeft = 0;
-            } else if (xc + mCurrentWidth / 2 > mOriginalWidth) {
-                mLeft = mOriginalWidth - mCurrentWidth;
-            } else {
-                mLeft = xc - mCurrentWidth / 2;
-            }
-
-            if (yc - mCurrentHeight / 2 < 0) {
-                mTop = 0;
-            } else if (yc + mCurrentHeight / 2 > mOriginalHeight) {
-                mTop = mOriginalHeight - mCurrentHeight ;
-            } else {
-                mTop = yc - mCurrentHeight /2;
-            }
-        }
-
-        fitPage();
-        updateImage();
     }
 
-    public void scroll(double distanceX, double distanceY) {
-        int dx = (int) distanceX, dy = (int) distanceY;
-        if (dx == 0 && dy == 0) {
-            return;
-        }
-        if (mLeft + mCurrentWidth + dx > mOriginalWidth) {
-            mLeft = mOriginalWidth - mCurrentWidth;
-        } else if (mLeft + dx < 0) {
-            mLeft = 0;
+    public void scroll(double dx, double dy) {
+        mOffsetX -= dx;
+        mOffsetY -= dy;
+
+        if (mOriginalWidth * mCurrentScale <= mScreenWidth) {
+            if (mOffsetX < 0) {
+                mOffsetX = 0;
+            } else if ((mOriginalWidth + mOffsetX) * mCurrentScale > mScreenWidth) {
+                mOffsetX = mScreenWidth / mCurrentScale - mOriginalWidth;
+            }
         } else {
-            mLeft += dx;
+            if (mOffsetX > 0) {
+                mOffsetX = 0;
+            } else if ((mOriginalWidth + mOffsetX) * mCurrentScale < mScreenWidth) {
+                mOffsetX = mScreenWidth / mCurrentScale - mOriginalWidth;
+            }
         }
-        if (mTop + mCurrentHeight + dy > mOriginalHeight) {
-            mTop = mOriginalHeight - mCurrentHeight;
-        } else if (mTop + dy < 0) {
-            mTop = 0;
+
+        if (mOriginalHeight * mCurrentScale <= mScreenHeight) {
+            if (mOffsetY < 0) {
+                mOffsetY = 0;
+            } else if ((mOriginalHeight + mOffsetY) * mCurrentScale > mScreenHeight) {
+                mOffsetY = mScreenHeight /mCurrentScale - mOriginalHeight;
+            }
         } else {
-            mTop += dy;
+            if (mOffsetY > 0) {
+                mOffsetY = 0;
+            } else if ((mOriginalHeight + mOffsetY) * mCurrentScale < mScreenHeight) {
+                mOffsetY = mScreenHeight /mCurrentScale - mOriginalHeight;
+            }
         }
-        updateImage();
+        invalidate();
     }
 
     /*
-     * This constant value is used to detect cases of significant difference between page and
-     * screen width/heigth proportion
-     */
-    private static final int ALLOWABLE_ERROR = 300;
-
-    public void fitPage(){
-        //zoom > 1 means showing whole page without fitting to page
-        if (mCurrentZoom <= 1 ) {
-            if (Math.abs(mCurrentWidth * mScreenHeight - mCurrentHeight * mScreenWidth)
-                    > ALLOWABLE_ERROR) {
-                int width = (int) (mOriginalWidth * mCurrentZoom),
-                        height = (int) (mOriginalHeight * mCurrentZoom);
-                if (width * mScreenHeight < height * mScreenWidth) {
-                    height = width * mScreenHeight / mScreenWidth;
-                } else {
-                    width = height * mScreenWidth / mScreenHeight;
-                }
-                mCurrentWidth = width;
-                mCurrentHeight = height;
-            }
-        } else {
-            mCurrentWidth = mOriginalWidth;
-            mCurrentHeight = mOriginalHeight;
-            mLeft = mTop = 0;
-        }
-    }
-
-    /*
-     * Loads page with same zoom factor as for previous page and shows leftmost topmost corner of it
+     * Loads page with same scale as for previous page and shows leftmost topmost corner of it
      */
     public void setPage(Bitmap page) {
-        mOriginalPage = page;
-        mOriginalHeight = page.getHeight();
+        mPage = page;
+        mOffsetX = mOffsetY = 0;
         mOriginalWidth = page.getWidth();
-        mLeft = 0;
-        mTop = 0;
-        mCurrentWidth = (int) (mOriginalWidth * mCurrentZoom);
-        mCurrentHeight = (int) (mOriginalHeight * mCurrentZoom);
-        fitPage();
-        updateImage();
+        mOriginalHeight = page.getHeight();
+        invalidate();
     }
 
-    /*
-     * Shows needed chunk of mOriginalPage
-     */
-    private void updateImage() {
-        int[] pixels = new int[mCurrentHeight * mCurrentWidth];
-        mOriginalPage.getPixels(pixels, 0, mCurrentWidth, mLeft, mTop, mCurrentWidth,
-                mCurrentHeight);
-        Bitmap page = Bitmap.createBitmap(pixels, mCurrentWidth, mCurrentHeight,
-                mOriginalPage.getConfig());
-        setImageBitmap(page);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mPage != null) {
+            if (mCurrentScale == 0) {
+                double cx = ((double ) mScreenWidth ) / ((double ) mOriginalWidth ),
+                       cy = ((double ) mScreenHeight ) / ( (double ) mOriginalHeight );
+                mCurrentScale = Math.max(cx, cy);
+            }
+            canvas.scale((float) mCurrentScale, (float) mCurrentScale);
+            canvas.translate((float) mOffsetX, (float ) mOffsetY);
+            canvas.drawBitmap(mPage, 0, 0, null);
+        }
     }
 }
